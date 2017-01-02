90f00e2e380bafa0086cd816327cd39691499dc3
Ivan Moore <ivan@teamoptimization.com>
2017-01-02 15:29:09 +0000

    added placeholder unit test to make it easier to get started

---
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -0,0 +1,13 @@
+package fi.iki.jka;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class JPhotoFrameTest {
+    @Test
+    public void placeholder() throws Exception {
+        assertThat(2, equalTo(2));
+    }
+}
